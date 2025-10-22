7e64edb2d718c295c16e0fdc042246ac2cce072d
Stan Switalski <stanley.switalski@dynatrace.com>
2025-10-22 04:20:05 +0000

    fix: adjusting post-create script for poetry dependencies

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,7 +2,7 @@
 
 # Configure Poetry for devcontainer
 poetry config virtualenvs.create false
-poetry install
+poetry install --project ./src
 
 # Initialize Kind Cluster
 kind create cluster --config .devcontainer/kind-cluster.yaml --wait 180s
